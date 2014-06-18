82a806c040f9e11b9927d9c2010f75464c5bb5e8
Rory McCann <rory@technomancy.org>
2014-06-18 16:48:30 +0100

    version numbers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="django-cache-by-user",
-      version="0.1",
+      version="1.0.0",
       author="Rory McCann",
       author_email="rory@technomancy.org",
       packages=['cache_by_user'],
