4029fad733eb7ec6f5795f8482660a6af1b5f19e
Low Kian Seong <kianseong@gmail.com>
2014-07-12 16:40:39 +0800

    Adding more metadata to setup.py and pep8 cleanup

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(name="django-cache-by-user",
       version="1.0.0",
       author="Rory McCann",
       author_email="rory@technomancy.org",
+      url='https://github.com/rory/django-cache-by-user',
       packages=['cache_by_user'],
-      license = 'GPLv3',
-      description = '',
-)
+      license='GPLv3',
+      description=('Make your django site share a cache'
+                   ' for all anonymous users, increasing cache hits'), )
